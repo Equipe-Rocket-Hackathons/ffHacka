0bc78f67cfb50af6546404cab11de0ae7e097699
ikaroamorim <ikaro.amorim@gmail.com>
2022-06-04 21:10:57 -0300

    Create item screen creation

--- src/screens/CreateItem/index.tsx
+++ src/screens/CreateItem/index.tsx
@@ -1,9 +1,56 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { Container } from './styles';
+import { CustomText } from '../../components/CustomText';
+import { MainButton } from '../../components/MainButton';
+import * as S from './styles';
 
 export const CreateItem: React.FC = () => {
+    const { navigate } = useNavigation()
+
     return (
-        <Container>
-        </Container>
+        <S.Container>
+
+            <S.Content>
+
+                <S.NewRegisterImage
+                    source={require('../../assets/new_register/new_register.png')}
+                    resizeMode='contain'></S.NewRegisterImage>
+
+                <CustomText
+                    size={26}
+                    align='center'
+                    color='text'
+                    weight='black'
+                    padding='50px 20px 0 20px'
+                >
+                    Adicione facilmente os itens de sua casa
+                </CustomText>
+
+                <CustomText
+                    size={14}
+                    align='center'
+                    color='text'
+                    weight='regular'
+                    padding='20px 35px 0 35px'
+                >
+                    Nosso scanner consegue identificar e adicionar automaticamente os itens do cômodo inteiro.
+                </CustomText>
+            </S.Content>
+
+        <S.ButtonSection>
+            <MainButton
+                isOutline
+                onPress={() => navigate('Home')}
+            >
+                Fazer captura manual
+            </MainButton>
+
+            <MainButton
+                onPress={() => navigate('Home')}
+            >
+                Escanear ambientes
+            </MainButton>
+            </S.ButtonSection>
+        </S.Container>
     );
 };
--- src/screens/CreateItem/styles.ts
+++ src/screens/CreateItem/styles.ts
@@ -1,5 +1,28 @@
 import styled from 'styled-components/native';
+import { Dimensions } from 'react-native';
+const { width } = Dimensions.get('window')
 
 export const Container = styled.View`
-  
+  flex: 1;
+  justify-content: space-around;
+  align-items: center;
 `;
+
+export const Content = styled.View`
+  width: 90%;
+`;
+
+export const ButtonSection = styled.View`
+   width: 90%;
+   display: flex;
+   align-items: center;
+   justify-content: space-between;
+   height: 136px;
+   margin: 25px 0 0 0;
+`;
+
+export const NewRegisterImage = styled.Image`
+  width: ${width}px;
+  margin-top: 70px;
+`;
+
